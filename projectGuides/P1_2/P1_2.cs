2dcd9f08581797ec3cd24864de4e417aea86b867
Willett, Spencer <spencer.willett@intel.com>
2021-08-25 11:16:52 -0700

    added hints as comments

--- projectGuides/P1_2/P1_2.cs
+++ projectGuides/P1_2/P1_2.cs
@@ -6,6 +6,7 @@ namespace P1_2
 {
     class Program
     {
+        // This function reads the inputs from the command line
         public static Tuple<string,string> getInputFromCommandLine(string[] args)
         {
             // initialize variables
@@ -22,6 +23,41 @@ namespace P1_2
             return Tuple.Create(input_1, input_2);
         }
 
+        // You can put your solution inside of this solve function
+        private static double Solve(string plaintext, string ciphertext)
+        {
+            // Helpful code from the instructions:
+            DateTime dt = DateTime.Now;
+            TimeSpan ts = dt.Subtract(new DateTime(1970, 1, 1));
+            // how to cast a TimeSpan to an int
+            // int start = (int)ts.TotalMinutes;
+
+            string secretString;
+            Random rng = new Random((int)ts.TotalMinutes);
+            byte[] key = BitConverter.GetBytes(rng.NextDouble());
+
+            Console.WriteLine(Encrypt(key, secretString));
+
+            // Hint: We are finding the seed that was used to make the key (and we only return the seed once we find the correct key that was used to encrypt the plaintext)
+            // The weakness is that C#'s Random((int)ts.TotalMinutes)) function is pseudo-random. Basically this means if we know the seed (int)ts.TotalMinutes then we can build the same random sequence used to make the key
+
+            return -1;
+        }
+
+        // The Encrypt function that your friend used
+        private static string Encrypt(byte[] key, string secretString)
+        {
+            DESCryptoServiceProvider csp = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, csp.CreateEncryptor(key, key), CryptoStreamMode.Write);
+            StreamWriter sw = new StreamWriter(cs);
+            sw.Write(secretString);
+            sw.Flush();
+            cs.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
         static void Main(string[] args)
         {
             // string plaintext = "Hello World";
@@ -35,10 +71,10 @@ namespace P1_2
             string ciphertext = commandlineInputs.Item2;
 
             // TODO: Code the solution
-            // var solution = Solve(plaintext, ciphertext);
+            var solution = Solve(plaintext, ciphertext);
 
             // Output the solution to the autograder
-            // Console.WriteLine(solution);
+            Console.WriteLine(solution);
         }
     }
 }
