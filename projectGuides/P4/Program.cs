71791082f9b3445c02c6b2f4f8af4448567a0de7
Willett, Spencer <spencer.willett@intel.com>
2021-09-13 17:25:45 -0700

    P4 starter code

---
+++ projectGuides/P4/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Numerics; // BigInteger
+using System.Collections.Generic;
+
+namespace P4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*
+            * useful help for RSA encrypt/decrypt: https://www.di-mgt.com.au/rsa_alg.html
+            * help with extended euclidean algorithm: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+            * 
+            */
+
+            // dotnet run 254 1223 251 1339 66536047120374145538916787981868004206438539248910734713495276883724693574434582104900978079701174539167102706725422582788481727619546235440508214694579  1756026041
+
+        }
+    }
+}
